9e96854bb70fcbe78f2e1515aafaec2a36b83fd3
yoelvp <yoelvp73@gmail.com>
2023-12-28 13:39:10 -0500

    modify: export new components

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,12 @@
 // Global styles
 export { GlobalStyles } from '@/ui/core/globalStyles'
 
+// Theme
+export { theme } from '@/ui/theme/theme'
+
 // Components
-export { Container } from '@/ui/components/container'
+export { Box, Flex } from '@/ui/components/box'
+export { Container, WrapperContent } from '@/ui/components/container'
+export { Header } from '@/ui/components/header'
+export { IconText } from '@/ui/components/icon-text'
+export { Link } from '@/ui/components/link'
