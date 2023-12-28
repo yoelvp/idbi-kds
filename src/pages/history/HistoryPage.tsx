becfa93990a4d4748344aac766d6e78895ea18f7
yoelvp <yoelvp73@gmail.com>
2023-12-28 11:40:48 -0500

    add: history page

---
+++ src/pages/history/HistoryPage.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react'
+
+import Layout from '@/layouts/Layout'
+
+const HistoryPage: FC = () => {
+  return (
+    <Layout>
+      <h1>History page</h1>
+    </Layout>
+  )
+}
+
+export default HistoryPage
