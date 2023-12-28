d498c977719367dcf3d7cb68cf37134282ab9ab0
yoelvp <yoelvp73@gmail.com>
2023-12-28 11:40:57 -0500

    add: orders page

---
+++ src/pages/orders/OrdersPage.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react'
+import Layout from '@/layouts/Layout'
+
+const OrdersPage: FC = () => {
+  return (
+    <Layout>
+      <h1>Order page</h1>
+      <p>Testing app</p>
+    </Layout>
+  )
+}
+
+export default OrdersPage
