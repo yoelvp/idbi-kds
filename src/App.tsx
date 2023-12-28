9e0eb4907c250f510049a9d695c9f3fd27c4bae9
yoelvp <yoelvp73@gmail.com>
2023-12-28 13:39:30 -0500

    fix: change of theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import type { FC } from 'react'
 
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from '@/ui/index'
 import { store } from '@/app/store'
+import { theme } from '@/ui'
 import Routing from '@/router/Routing'
-import { ThemeProvider } from 'styled-components'
 
 const App: FC = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <GlobalStyles />
-        <ThemeProvider theme={{ bg: 'red', fg: 'blue' }}>
+        <ThemeProvider theme={theme}>
           <Routing />
         </ThemeProvider>
       </BrowserRouter>
