f978d963c191778501a57112dedc9e302a4c0876
yoelvp <yoelvp73@gmail.com>
2023-12-29 17:10:36 -0500

    move key to env file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD67cVUI73IveIOReFpfA5pkkfcQMt52W8',
-  authDomain: 'challenge-idbi.firebaseapp.com',
-  databaseURL: 'https://challenge-idbi-default-rtdb.firebaseio.com',
-  projectId: 'challenge-idbi',
-  storageBucket: 'challenge-idbi.appspot.com',
-  messagingSenderId: '733400524452',
-  appId: '1:733400524452:web:aabda38c4e7415be21a831'
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
