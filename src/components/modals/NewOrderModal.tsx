cee4a67787d9afe4c611d3fcf3e48a80b6176967
yoelvp <yoelvp73@gmail.com>
2023-12-29 13:13:24 -0500

    modal of add new order

--- src/components/modals/NewOrderModal.tsx
+++ src/components/modals/NewOrderModal.tsx
@@ -1,19 +1,48 @@
-import { useState, type FC } from 'react'
+import type { FC } from 'react'
 
+import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
 import Modal from '@/components/Modal'
 import { Flex, Input } from '@/ui'
-import { ButtonAction } from '@/ui/components/button'
+import { Button, ButtonAction } from '@/ui/components/button'
 import { IoMdAdd } from 'react-icons/io'
+import { useDispatch } from 'react-redux'
+import { addNewOrder } from '@/features/orders/orderSlice'
+import { onClose } from '@/features/modals/newOrderModalSlice'
 
 const NewOrderModal: FC = () => {
   const [numOrders, setNumOrders] = useState(1)
+  const dispatch = useDispatch()
 
   const handleAddOrder = (): void => {
     setNumOrders(numOrders + 1)
   }
 
+  const handleCloseOrderModal = (): void => {
+    dispatch(onClose())
+  }
+
+  const addOrder = (): void => {
+    const newOrder = {
+      id: uuid(),
+      time: '11:32',
+      date: '2023-12-29T11:32:65',
+      status: 'En proceso',
+      orders: [
+        {
+          name: 'Chicken',
+          quantity: 2,
+          unit: 4,
+          details: 'Mayonesa extra'
+        }
+      ]
+    }
+
+    dispatch(addNewOrder(newOrder))
+  }
+
   return (
-    <Modal title="Nuevo pedido">
+    <Modal title="Nuevo pedido" onClose={handleCloseOrderModal}>
       <form>
         <Flex $flexDirection="column" $gap="24">
           {[...Array(numOrders)].map((_, index) => (
@@ -29,8 +58,8 @@ const NewOrderModal: FC = () => {
                 <ButtonAction
                   onClick={handleAddOrder}
                   type="button"
-                  $bg="yellow.200"
-                  $color="blue.800"
+                  $bg="yellow-200"
+                  $color="blue-800"
                 >
                   <IoMdAdd size="18" />
                 </ButtonAction>
@@ -46,6 +75,8 @@ const NewOrderModal: FC = () => {
           <option value="Cupcake"></option>
         </datalist>
       </form>
+
+      <Button onClick={addOrder}>Agregar nuevo pedido</Button>
     </Modal>
   )
 }
