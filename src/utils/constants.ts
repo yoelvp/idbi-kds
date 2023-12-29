7d8de926af3bf7ae2344e040f4b2cb759536efc1
yoelvp <yoelvp73@gmail.com>
2023-12-29 17:11:33 -0500

    add order status in const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,9 @@ export const ReduxSliceNames = {
   tasks: 'TASKS',
   newOrderModal: 'NEW_ORDER_MODAL'
 }
+
+export const OrderStatus = {
+  FINALIZED: 'Finalizado',
+  REGISTERED: 'Registrado',
+  IN_PROGRESS: 'En proceso'
+}
