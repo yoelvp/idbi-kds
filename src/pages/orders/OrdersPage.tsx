94637a015adbcd35dd484566c381d25457bc36d8
yoelvp <yoelvp73@gmail.com>
2023-12-29 16:06:33 -0500

    refactor: move code

--- src/pages/orders/OrdersPage.tsx
+++ src/pages/orders/OrdersPage.tsx
@@ -2,14 +2,14 @@ import type { FC } from 'react'
 import type { Order } from '@/schemes/order'
 
 import { useEffect } from 'react'
-import Layout from '@/layouts/Layout'
-import { Grid } from '@/ui/components/grid'
 import { onValue, ref } from 'firebase/database'
 import { useDispatch, useSelector } from 'react-redux'
-import { database } from '@/lib/firebase'
 import { initialize } from '@/features/orders/orderSlice'
+import Layout from '@/layouts/Layout'
 import OrderCard from '@/components/OrderCard'
 import NewOrderModal from '@/components/modals/NewOrderModal'
+import { Grid } from '@/ui'
+import { database } from '@/lib/firebase'
 
 const OrdersPage: FC = () => {
   const dispatch = useDispatch()
@@ -22,25 +22,8 @@ const OrdersPage: FC = () => {
     onValue(query, (snap) => {
       const data: Record<string, Order> = snap.val()
 
-      const newData: Order[] = Object.values(data).map(d => {
-        const newOrders = Object.values(d.orders).map(ord => ({
-          name: ord.name,
-          quantity: ord.quantity,
-          unit: ord.unit,
-          details: ord.details
-        }))
-
-        return {
-          id: d.id,
-          date: d.date,
-          time: d.time,
-          status: d.status,
-          orders: newOrders
-        }
-      })
-
       if (snap.exists()) {
-        dispatch(initialize(newData))
+        dispatch(initialize(data))
       }
     })
   }, [dispatch])
@@ -48,9 +31,11 @@ const OrdersPage: FC = () => {
   return (
     <Layout>
       <Grid $gap="24">
-        {orders.map((order: Order) => (
-          <OrderCard key={order.id} order={order} />
-        ))}
+        {orders
+          .filter((order: Order) => order.status !== 'finalizado')
+          .map((order: Order) => (
+            <OrderCard key={order.id} order={order} />
+          ))}
       </Grid>
 
       {isOpenModal && <NewOrderModal />}
