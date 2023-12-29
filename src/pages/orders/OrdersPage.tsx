aaf513545e62fd6b127c32e812e10599ff83833e
yoelvp <yoelvp73@gmail.com>
2023-12-29 17:11:09 -0500

    refactor: pass props to OrderCard component

--- src/pages/orders/OrdersPage.tsx
+++ src/pages/orders/OrdersPage.tsx
@@ -4,12 +4,13 @@ import type { Order } from '@/schemes/order'
 import { useEffect } from 'react'
 import { onValue, ref } from 'firebase/database'
 import { useDispatch, useSelector } from 'react-redux'
-import { initialize } from '@/features/orders/orderSlice'
+import { initialize, changeOrderStatus } from '@/features/orders/orderSlice'
 import Layout from '@/layouts/Layout'
 import OrderCard from '@/components/OrderCard'
 import NewOrderModal from '@/components/modals/NewOrderModal'
 import { Grid } from '@/ui'
 import { database } from '@/lib/firebase'
+import { OrderStatus } from '@/utils/constants'
 
 const OrdersPage: FC = () => {
   const dispatch = useDispatch()
@@ -28,13 +29,26 @@ const OrdersPage: FC = () => {
     })
   }, [dispatch])
 
+  const updateOrderToProcess = ({ order }: { order: Order }): void => {
+    dispatch(changeOrderStatus({ ...order, status: OrderStatus.IN_PROGRESS }))
+  }
+
+  const changeStatusCompletedOrder = ({ order }: { order: Order }): void => {
+    dispatch(changeOrderStatus({ ...order, status: OrderStatus.FINALIZED }))
+  }
+
   return (
     <Layout>
       <Grid $gap="24">
         {orders
-          .filter((order: Order) => order.status !== 'finalizado')
+          .filter((order: Order) => order.status !== OrderStatus.FINALIZED)
           .map((order: Order) => (
-            <OrderCard key={order.id} order={order} />
+            <OrderCard
+              key={order.id}
+              order={order}
+              updateOrderToProcess={updateOrderToProcess}
+              changeStatusCompletedOrder={changeStatusCompletedOrder}
+            />
           ))}
       </Grid>
 
