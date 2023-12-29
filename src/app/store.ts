aaccd124310f67fe20b2572824e15c5c63b9caaf
yoelvp <yoelvp73@gmail.com>
2023-12-29 01:36:59 -0500

    modify: add order

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import orderSlice from '@/features/orders/orderSlice'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    orders: orderSlice
+  }
 })
 
 export type RootState = ReturnType<typeof store.getState>
