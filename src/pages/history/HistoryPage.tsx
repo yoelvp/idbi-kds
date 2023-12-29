921e5bbb37fa86932590ca30bc330eee1d0865ca
yoelvp <yoelvp73@gmail.com>
2023-12-29 17:10:50 -0500

    modify: add orders completed

--- src/pages/history/HistoryPage.tsx
+++ src/pages/history/HistoryPage.tsx
@@ -1,11 +1,23 @@
 import type { FC } from 'react'
+import type { Order } from '@/schemes/order'
 
+import { useSelector } from 'react-redux'
 import Layout from '@/layouts/Layout'
+import OrderCard from '@/components/OrderCard'
+import { Grid } from '@/ui'
+import { OrderStatus } from '@/utils/constants'
 
 const HistoryPage: FC = () => {
+  const orders = useSelector((state: any) => state.orders.orders)
   return (
     <Layout>
-      <h1>History page</h1>
+      <Grid $gap="24">
+        {orders
+          .filter((order: Order) => order.status === OrderStatus.FINALIZED)
+          .map((order: Order) => (
+            <OrderCard key={order.id} order={order} />
+          ))}
+      </Grid>
     </Layout>
   )
 }
