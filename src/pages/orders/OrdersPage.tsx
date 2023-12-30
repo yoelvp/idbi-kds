53fb41128390909f4d698c697d186ade3152cd0c
yoelvp <yoelvp73@gmail.com>
2023-12-29 22:49:45 -0500

    change mapping

--- src/pages/orders/OrdersPage.tsx
+++ src/pages/orders/OrdersPage.tsx
@@ -4,17 +4,22 @@ import type { Order } from '@/schemes/order'
 import { useEffect } from 'react'
 import { onValue, ref } from 'firebase/database'
 import { useDispatch, useSelector } from 'react-redux'
-import { initialize, changeOrderStatus } from '@/features/orders/orderSlice'
+import {
+  initialize,
+  changeOrderStatus,
+  cancelOrder
+} from '@/features/orders/orderSlice'
 import Layout from '@/layouts/Layout'
 import OrderCard from '@/components/OrderCard'
 import NewOrderModal from '@/components/modals/NewOrderModal'
-import { Grid } from '@/ui'
+import { Box, Flex, Grid } from '@/ui'
 import { database } from '@/lib/firebase'
 import { OrderStatus } from '@/utils/constants'
+import { selectFilteredOrders } from '@/features/orders/ordersFilter'
 
 const OrdersPage: FC = () => {
   const dispatch = useDispatch()
-  const orders = useSelector((state: any) => state.orders.orders)
+  const orders = useSelector(selectFilteredOrders)
   const isOpenModal = useSelector((state: any) => state.orderModal.isOpen)
 
   useEffect(() => {
@@ -29,27 +34,44 @@ const OrdersPage: FC = () => {
     })
   }, [dispatch])
 
-  const updateOrderToProcess = ({ order }: { order: Order }): void => {
+  const handleMakeOrder = ({ order }: { order: Order }): void => {
     dispatch(changeOrderStatus({ ...order, status: OrderStatus.IN_PROGRESS }))
   }
 
-  const changeStatusCompletedOrder = ({ order }: { order: Order }): void => {
+  const handleCompleteOrder = ({ order }: { order: Order }): void => {
     dispatch(changeOrderStatus({ ...order, status: OrderStatus.FINALIZED }))
+  }
+
+  const handleCancelOrder = ({ order }: { order: Order }): void => {
+    dispatch(cancelOrder({ ...order, status: OrderStatus.CANCELLED }))
   }
 
   return (
     <Layout>
+      {orders.length < 1 && (
+        <Flex $justifyContent="center" $alignItems="center">
+          <Box
+            $bg="blue-600"
+            $width="280px"
+            $rounded="16"
+            $padding="24"
+            $textAlign="center"
+          >
+            No hay pedidos
+          </Box>
+        </Flex>
+      )}
+
       <Grid $gap="24">
-        {orders
-          .filter((order: Order) => order.status !== OrderStatus.FINALIZED)
-          .map((order: Order) => (
-            <OrderCard
-              key={order.id}
-              order={order}
-              updateOrderToProcess={updateOrderToProcess}
-              changeStatusCompletedOrder={changeStatusCompletedOrder}
-            />
-          ))}
+        {orders.map((order: Order) => (
+          <OrderCard
+            key={order.id}
+            order={order}
+            handleMakeOrder={handleMakeOrder}
+            handleCompleteOrder={handleCompleteOrder}
+            handleCancelOrder={handleCancelOrder}
+          />
+        ))}
       </Grid>
 
       {isOpenModal && <NewOrderModal />}
